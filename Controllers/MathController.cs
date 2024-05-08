4f69fb493e6ce3877825d18654139b18443c34d4
vehap24 <bvehap46@gmail.com>
2024-05-08 08:18:37 +0200

    feature: Aggiunto file MathController.cs

---
+++ Controllers/MathController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AS2324_5G_INF_BunardziuVehap_webAPI.Controllers
+{
+    public class MathController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
