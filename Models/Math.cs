a77d04bf391860f0ea4dd6bb2f066c253c5c77f6
vehap24 <bvehap46@gmail.com>
2024-05-08 08:32:55 +0200

    fix: Cambiato nome Math.cs a Operazioni.cs per evitare ambiguità

--- Models/Math.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace AS2324_5G_INF_BunardziuVehap_webAPI.Models
-{
-    public class Math
-    {
-    }
-}
---
+++ Models/Operazioni.cs
@@ -0,0 +1,23 @@
+﻿namespace AS2324_5G_INF_BunardziuVehap_webAPI.Models
+{
+    public class Operazioni
+    {
+        public int N1 { get; set; }
+        public int N2 { get; set; }
+        public bool Multiplo => N2 % N1 == 0;
+        public int Potenza(int N1, int N2)
+        {
+            int risultato = 1;
+            for(int i = 0; i < N2; i++)
+            {
+                risultato = risultato * i;
+            }
+            return risultato;
+        }
+        public bool AnnoBisestile => N1 % 4 == 0;
+
+        public int Ipotenusa(int N1, int N2) {
+            ((N1 * N1) + (N2 * N2));
+        }
+    }
+}
